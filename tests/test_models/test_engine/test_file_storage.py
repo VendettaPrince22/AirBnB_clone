3c87f6ff391beddbced182510a13e3518bd25ecf
Kairo Prince <pomegah@gmail.com>
2024-07-15 11:47:57 -0400

    Pushing test files

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,10 +1,16 @@
 import unittest
+import json
 from models.engine.file_storage import FileStorage
 
+
 class TestFileStorage(unittest.TestCase):
-    
-    def test_init(self):
-        pass
+
+    def test_all(self):
+        storage = FileStorage()
+        with open("file.json", 'r') as f:
+            my_dict = json.load(f)
+        self.assertEqual(len(storage.all()), len(my_dict))
+
 
 if __name__ == "__main__":
     unittest.main()
