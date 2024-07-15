027ddae99f993c9f418f2bf798a814f8d875fec5
Kairo Prince <pomegah@gmail.com>
2024-07-15 10:37:17 -0400

    Pushing test files

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,14 +2,15 @@ import unittest
 import json
 from models.base_model import BaseModel
 
+
 class TestBaseModel(unittest.TestCase):
 
     def setUp(self):
         self.my_model = BaseModel()
-    
+
     def test_init(self):
         self.assertIsInstance(self.my_model, BaseModel)
-    
+
     def test_save(self):
         self.my_model.save()
         self.assertNotEqual(self.my_model.updated_at, self.my_model.created_at)
@@ -17,6 +18,10 @@ class TestBaseModel(unittest.TestCase):
         with open("file.json", 'r') as f:
             my_dict = json.load(f)
             self.assertIn(model_id, my_dict)
+
+    def test_to_dict(self):
+        pass
+
 
 if __name__ == "__main__":
     unittest.main()
