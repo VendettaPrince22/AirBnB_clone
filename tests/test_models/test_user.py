a31aa1ad8827a6bad5f0183124766ae382192732
Kairo Prince <pomegah@gmail.com>
2024-07-15 12:07:29 -0400

    Pushing test files

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,10 +1,13 @@
 import unittest
 from models.user import User
 
+
 class TestUser(unittest.TestCase):
-    
-    def test_init(self):
-        pass
+
+    def test_email(self):
+        new_user = User()
+        self.assertEqual(new_user.email, "")
+
 
 if __name__ == "__main__":
     unittest.main()
