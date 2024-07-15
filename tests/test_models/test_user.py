6b7576281c9a83d53a68f839b6de20d3cb78af43
Kairo Prince <pomegah@gmail.com>
2024-07-15 12:13:22 -0400

    Pushing test files

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -4,9 +4,20 @@ from models.user import User
 
 class TestUser(unittest.TestCase):
 
+    def setUp(self):
+        self.new_user = User()
+
     def test_email(self):
-        new_user = User()
-        self.assertEqual(new_user.email, "")
+        self.assertEqual(self.new_user.email, "")
+
+    def test_password(self):
+        self.assertEqual(self.new_user.password, "")
+
+    def test_first_name(self):
+        self.assertEqual(self.new_user.first_name, "")
+
+    def test_last_name(self):
+        self.assertEqual(self.new_user.last_name, "")
 
 
 if __name__ == "__main__":
