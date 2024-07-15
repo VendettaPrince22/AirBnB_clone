ff1f7ae608b1e33e47d2822e92a05cf8df415a79
Kairo Prince <pomegah@gmail.com>
2024-07-15 09:52:29 -0400

    Pushing test files

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,11 +2,16 @@ import unittest
 from models.base_model import BaseModel
 
 class TestBaseModel(unittest.TestCase):
+
+    def setUp(self):
+        self.my_model = BaseModel()
     
     def test_init(self):
-        my_model = BaseModel()
-
-        self.assertIsInstance(my_model, BaseModel)
+        self.assertIsInstance(self.my_model, BaseModel)
+    
+    def test_save(self):
+        self.my_model.save()
+        self.assertNotEqual(self.my_model.updated_at, self.my_model.created_at)
 
 if __name__ == "__main__":
     unittest.main()
